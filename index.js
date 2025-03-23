285682c2fb64685efc55fc5bfe0f53ac416a78ab
MiglenaD-tech <MDodleva@vertinitylabs.com>
2025-03-23 11:04:19 +0200

    Add Amplify config to fix dist error

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => res.send("Hello World from AWS!"));
+//app.get("/", (req, res) => res.send("Hello World from AWS!"));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// Зареждай HTML файла от папка public
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "helloWorldAws.html"));
+});
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
